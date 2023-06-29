3d2b346f053bad3f30caeac25de75d020575e951
fascilio <fascilior@gmail.com>
2023-06-29 22:55:44 +0300

    authorized members

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,31 @@
-class SessionsController < ApplicationController
+# class SessionsController < ApplicationController
   
+#   def create
+#     user = User.find_by(username: params[:username])
+#     session[:user_id] = user.id
+#     render json: user
+#   end
+
+#   def destroy
+#     session.delete :user_id
+#     head :no_content
+#   end
+
+# end
+class SessionsController < ApplicationController
   def create
-    user = User.find_by(username: params[:username])
-    session[:user_id] = user.id
-    render json: user
+    user = User.new(username: params[:username])
+
+    if user.save
+      session[:user_id] = user.id
+      render json: user
+    else
+      render json: { error: 'Invalid username' }, status: 401
+    end
   end
 
   def destroy
-    session.delete :user_id
+    session.delete(:user_id)
     head :no_content
   end
-
 end
